354be572c7a9b29e4b72011fe5b7345b7d088bac
Jörn Ott <joern.ott@ott-consult.de>
2022-04-28 10:40:04 +0200

    first commit

---
+++ _build/version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# see https://github.com/joernott/monitoring-check_iostats/tags
+APP="monitoring-check_linux_net"
+VERSION="1.0.0"
+RELEASE="1"
+URL="https://github.com/joernott/Nag_checks/archive/refs/tags/v${VERSION}.tar.gz"
+ARCH="noarch"
