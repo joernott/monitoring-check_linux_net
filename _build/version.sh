8c0c3aa30300bf776d85f7b42577ed4d67d77442
Joern Ott <joern.ott@ott-consult.de>
2022-10-04 17:41:35 +0200

    Bump version

--- _build/version.sh
+++ _build/version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # see https://github.com/joernott/monitoring-check_linux_net/tags
 APP="monitoring-check_linux_net"
-VERSION="1.0.0"
+VERSION="1.0.1"
 RELEASE="1"
 URL="https://github.com/joernott/monitoring-check_linux_net/archive/refs/tags/v${VERSION}.tar.gz"
 ARCH="noarch"
